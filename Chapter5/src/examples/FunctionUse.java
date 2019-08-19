1ff4aec00025d205b7fe7ea9775285318e385d4b
Abdelmonaim Salhaoui <salhaoui.abdelmonaim>
2019-08-19 15:48:25 +0200

    Primitive Versions of Functional Interfaces

--- Chapter5/src/examples/FunctionUse.java
+++ Chapter5/src/examples/FunctionUse.java
@@ -2,13 +2,17 @@ package examples;
 
 import java.util.Arrays;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 
 public class FunctionUse {
 
 	public static void main(String[] args) {
 
-		Function<String, Integer> strLength = str -> str.length();
-		System.out.println(strLength.apply("Hallo Leute"));
+		// Function<Integer, Integer> test1 = Math::abs;
+
+		// Primitive Version
+		IntFunction test1 = Math::abs;
+		System.out.println(test1.apply(-10));
 	}
 
 }
