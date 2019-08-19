35bd5f0f7118423dcbc934b5c6f946569ee5384a
Abdelmonaim Salhaoui <salhaoui.abdelmonaim>
2019-08-19 15:59:16 +0200

    Primitive Versions of Consumer Interface

--- Chapter5/src/examples/ConsumerTest.java
+++ Chapter5/src/examples/ConsumerTest.java
@@ -1,15 +1,19 @@
 package examples;
 
 import java.util.function.Consumer;
+import java.util.function.IntConsumer;
 import java.util.stream.Stream;
 
 public class ConsumerTest {
 
 	public static void main(String[] args) {
 
-		Consumer<String> printUpperCase = 
-				str -> System.out.println(str.toUpperCase());
+		Consumer<String> printUpperCase = str -> System.out.println(str.toUpperCase());
 		printUpperCase.accept("hello");
+
+		// Primitive Version of Consumer Interface Example
+		IntConsumer test = a -> System.out.println(a + 2);
+		test.accept(2);
 	}
 
 }
